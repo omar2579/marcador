ecf822f0d74e22b217c7af3310fe62f966a0f327
omar <mann002579@gmail.com>
2014-07-09 00:38:03 -0500

    Marcador java
    el bueno

--- src/marcador/marcador_java.java
+++ src/marcador/marcador_java.java
@@ -15,8 +15,9 @@ public class marcador_java extends javax.swing.JFrame {
      */
     public marcador_java() {
         initComponents();
+        jButton3.setVisible(false);
     }
-
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -29,6 +30,7 @@ public class marcador_java extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -53,6 +55,14 @@ public class marcador_java extends javax.swing.JFrame {
             }
         });
 
+        jButton3.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jButton3.setText("Recet");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -64,8 +74,13 @@ public class marcador_java extends javax.swing.JFrame {
                 .addComponent(jButton2)
                 .addGap(81, 81, 81))
             .addGroup(layout.createSequentialGroup()
-                .addGap(95, 95, 95)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 212, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(95, 95, 95)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 212, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(153, 153, 153)
+                        .addComponent(jButton3)))
                 .addContainerGap(93, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -77,7 +92,9 @@ public class marcador_java extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton1)
                     .addComponent(jButton2))
-                .addContainerGap(113, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton3)
+                .addContainerGap(84, Short.MAX_VALUE))
         );
 
         pack();
@@ -94,6 +111,15 @@ public class marcador_java extends javax.swing.JFrame {
         String puntos=score();
         jLabel1.setText(puntos);
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        player1=0;
+        player2=0;
+        jLabel1.setText("LOVE - ALL");
+        jButton1.setEnabled(true);
+        jButton2.setEnabled(true);
+        jButton3.setVisible(false);
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -130,8 +156,8 @@ public class marcador_java extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new marcador_java().setVisible(true);
-            }
-        });
+            } 
+        });        
     }
     public void point(int jugador){
         switch(jugador){
@@ -161,11 +187,29 @@ public class marcador_java extends javax.swing.JFrame {
         }
         if(player1==5){
             player1=0;player2=0;
+            jButton1.setEnabled(false);
+            jButton2.setEnabled(false);
+            jButton3.setVisible(true);
              return "PLAYER 1 WINS";
         }
         if(player2==5){
             player1=0;player2=0;
+            jButton1.setEnabled(false);
+            jButton2.setEnabled(false);
+            jButton3.setVisible(true);
              return "PLAYER 2 WINS";
+        }
+        if(player1==4 && player2 <3){
+            jButton1.setEnabled(false);
+            jButton2.setEnabled(false);
+            jButton3.setVisible(true);
+            return "PLAYER 1 WINS";
+        }
+        if(player1<3 && player2 ==4){
+            jButton1.setEnabled(false);
+            jButton2.setEnabled(false);
+            jButton3.setVisible(true);
+            return "PLAYER 2 WINS";
         }
         else
             return puntos[player1]+" - "+puntos[player2];
@@ -174,6 +218,7 @@ public class marcador_java extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
